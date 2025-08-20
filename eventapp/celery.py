f5dfe9262925b48b2dc6615168694c0a46d8f20b
tdttrang <duongtrangthuy147012@gmail.com>
2025-08-20 21:58:44 +0700

    doi cho evenlet

--- eventapp/celery.py
+++ eventapp/celery.py
@@ -1,11 +1,12 @@
 # eventapp_project/celery.py
 
+import eventlet
+eventlet.monkey_patch()
+
 import os
 from celery import Celery
-import eventlet # Nhập eventlet
 
 os.environ.setdefault('FORK_MODE', 'spawn')
-eventlet.monkey_patch() # Thêm dòng này
 
 # Dat bien moi truong de Django biet su dung settings.py
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eventapp.settings')
--- eventapp/wsgi.py
+++ eventapp/wsgi.py
@@ -6,6 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
+# them vao cho railway chay
+import eventlet
+eventlet.monkey_patch()
 
 import os
 
