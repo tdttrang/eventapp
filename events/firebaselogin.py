cd2c0e77fab8dcd25b0fda2af30c43f21a8d7eea
tdttrang <duongtrangthuy147012@gmail.com>
2025-09-11 11:03:09 +0700

    firebase

--- events/firebaselogin.py
+++ events/firebaselogin.py
@@ -2,6 +2,7 @@ from oauth2_provider.oauth2_validators import OAuth2Validator
 from google.oauth2 import id_token
 from google.auth.transport import requests
 from django.contrib.auth import get_user_model
+from firebase_admin import auth
 
 class FirebaseOAuth2Validator(OAuth2Validator):
     def validate_grant_type(self, grant_type, client, request, *args, **kwargs):
@@ -17,10 +18,9 @@ class FirebaseOAuth2Validator(OAuth2Validator):
     def validate_user(self, request, *args, **kwargs):
         token = request.extra_credentials.get('token')
         try:
-            # Xác thực Firebase ID token
-            idinfo = id_token.verify_oauth2_token(token, requests.Request())
-            email = idinfo['email']
-            name = idinfo.get('name', '')
+            decoded_token = auth.verify_id_token(token)
+            email = decoded_token.get('email')
+            name = decoded_token.get('name', '')
             User = get_user_model()
             user, _ = User.objects.get_or_create(
                 username=email,
@@ -28,5 +28,6 @@ class FirebaseOAuth2Validator(OAuth2Validator):
             )
             request.user = user
             return True
-        except Exception:
+        except Exception as e:
+            print("Firebase token verify failed:", e)
             return False
